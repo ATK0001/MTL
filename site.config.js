a7594166fec9d0e41e78612673d88a3d2c750f98
ATK0001 <33389190+ATK0001@users.noreply.github.com>
2025-05-12 20:57:57 +1000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Terrain IQ",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
+    role: "",
+    bio: "Your Intelligent Guide to 4WD Tracks",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
@@ -12,14 +12,14 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Terrain IQ",
+    description: "Your Intelligent Guide to 4WD Tracks",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
